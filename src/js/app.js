ae67644c6f9593c1073f63c3ea09955373dacfc7
Yacine Benzerroug <yassine.benzerroug@gmail.com>
2021-12-10 20:39:32 +0100

    ⚡  REFACTOR: style enhancement

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,20 @@
 import copy from 'clipboard-copy';
 
-
 function init() {
   let cta = document.querySelector(".cta");
   cta.addEventListener('click', function (e) {
     e.preventDefault();
     copy('hello@yacine.design');
+    cta.classList.add('copied');
+    cta.disabled = true;
+    setTimeout(function () {
+      cta.classList.remove('copied');
+      console.log("class removed");
+    }, 600);
   });
 }
-
-
-
-
 
 window.addEventListener('load', function () {
   document.querySelector('body').classList.remove("loading");
   init();
 });
-
-
