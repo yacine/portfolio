f05b45016472fe67bda73bead555c6a154171c8c
Yacine Benzerroug <yassine.benzerroug@gmail.com>
2021-12-13 23:11:11 +0100

    ⚡  REFACTOR: Style and js

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,18 @@
 import copy from 'clipboard-copy';
 
 function init() {
-  let cta = document.querySelector(".cta");
+  let cta = document.querySelector(".cta"),
+  email = document.querySelector(".cta").textContent;
   cta.addEventListener('click', function (e) {
     e.preventDefault();
-    copy('hello@yacine.design');
+    copy(email);
     cta.classList.add('copied');
+    cta.textContent = "Email copied 👌"
     cta.disabled = true;
     setTimeout(function () {
       cta.classList.remove('copied');
       console.log("class removed");
+      cta.textContent = email;
     }, 600);
   });
 }
